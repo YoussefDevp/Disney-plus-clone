b6e00c50367501c52b638c1de3e8f0e94a88dbcc
YoussefDevp <youssefmed212@gmail.com>
2021-05-04 02:21:26 +0200

    Add Detail page

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -13,18 +13,26 @@ function Detail() {
 
             <Controls>
                 <PlayButton>
-                
+                    <img src="/images/play-icon-black.png" />
+                    <span>PLAY</span>
                 </PlayButton>
                 <TrailerButton>
-                    
+                <img src="/images/play-icon-white.png" />
+                    <span>TRAILER </span>
                 </TrailerButton>
                 <AddButton>
-
+                    <span>+</span>
                 </AddButton>
                 <GroupWatchButton>
-                            
+                       <img src="/images/group-icon.png" />     
                 </GroupWatchButton> 
             </Controls>
+            <SubTitle>
+                2018 * 7m * Family, Fanstasy, Kids, Animation
+            </SubTitle>
+            <Description>
+            Toy Story 4 sees the return of Woody, Buzz and the whole gang, far from home, discovering new friends—and old ones—on an eye-opening road trip that takes them to unexpected places.
+            </Description>
         </Container>
     )
 }
@@ -33,7 +41,7 @@ export default Detail
 
 const Container = styled.div`
     min-height: calc(100vh - 70px);
-    padding: 0 calc(3.5vw + 5px);
+    padding:  calc(3.5vw + 5px);
     position: relative;
 `
 const Background = styled.div`
@@ -65,17 +73,65 @@ const ImgTitle = styled.div`
 `
 
 const Controls = styled.div`
+   display: flex;
+   align-items: center;
 `
 
-const PlayButton = styled.div`
+const PlayButton = styled.button`
+   border-radius: 5px;
+   display: flex;
+   align-items: center;
+   padding: 0px 24px;
+   margin-right: 22px;
+   height: 56px;
+   background: rgb(249, 249, 249);
+   border: none;
+   letter-spacing: 1.8px;
+   cursor: pointer;
+
+   &:hover {
+    background: rgb(198, 198, 198);
+   }
 ` 
 
-const TrailerButton = styled.div`
+const TrailerButton = styled(PlayButton)`
+   background: rgba(0, 0, 0, 0.3);
+   border: 1px solid rgb(249, 249, 249);
+   color: rgb(249, 249, 249); 
 `
 
-const AddButton = styled.div`
+const AddButton = styled.button`
+   margin-right: 16px;
+   width: 44px;
+   height: 44px;
+   display: flex;
+   align-items: center;
+   justify-content: center;
+   border-radius: 50%;
+   border: 2px solid white;
+   background-color: rgba(0, 0, 0, 0.6);
+   cursor: pointer;
+
+   span {
+       font-size: 30px;
+       color: white;
+   }
 `
 
-const GroupWatchButton = styled.div`
+const GroupWatchButton = styled(AddButton)`
+   background: rgb(0,0,0);
+`
 
+const SubTitle = styled.div`
+   color: rgb(249, 249, 249);
+   font-size: 15px;
+   min-height: 20px;
+   margin-top: 26px;
+`
+
+const Description = styled.div`
+   line-height: 1.4;
+   font-size: 20px;
+   margin-top: 16px;
+   color: rgb(249, 249, 249);
 `
