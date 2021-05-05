5f9a4d99508d913214fd380a34bfc70ad2ead6e0
YoussefDevp <youssefmed212@gmail.com>
2021-05-06 01:29:46 +0200

    Grab detail page from db

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Link } from "react-router-dom"
 import { selectMovies } from "../features/movie/movieSlice"
 import { useSelector } from "react-redux"
 
 function Movies() {
     const movies = useSelector(selectMovies)
-
-    console.log("this is movies",movies);
     return (
         <Container>
             <h4>Recommended for You</h4>
@@ -14,7 +13,9 @@ function Movies() {
                 { movies &&
                     movies.map((movie)=>(
                         <Wrap key= {movie.id}>
+                            <Link to={`/detail/${movie.id}`}>
                             <img src={movie.cardImg} />
+                            </Link>
                         </Wrap>
                     ))
                 }
